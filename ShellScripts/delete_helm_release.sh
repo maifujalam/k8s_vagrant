d05c1d32542e84a8a0e409d470da1697d03437c2
maifujalam <sk.maifujalam@gmail.com>
2024-08-31 16:04:58 +0530

    argocd

--- ShellScripts/delete_helm_release.sh
+++ ShellScripts/delete_helm_release.sh
@@ -3,3 +3,4 @@ helm -n kube-vip uninstall kube-vip-cloud-provider
 helm -n kube-vip uninstall kube-vip
 helm -n cert-manager uninstall cert-manager
 helm -n tigera-operator uninstall calico
+helm -n ingress-nginx uninstall ingress-nginx
